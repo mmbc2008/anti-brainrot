a49b9d9e746afc44889dcff27870bbab43969382
Mylène <m-b-c@mail.com>
2026-05-13 20:24:53 +0200

    feat: added SQLite schema and connection helper.

---
+++ db.py
@@ -0,0 +1,56 @@
+import sqlite3
+from contextlib import contextmanager
+
+@contextmanager
+def get_connection():
+    conn = sqlite3.connect("data/events.db")
+    
+    try:
+        yield conn
+        conn.commit()
+    except:
+        conn.rollback()
+        raise
+    finally:
+        conn.close()
+
+def init_db(conn):
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS organisers(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        last_scraped_at TEXT,
+        profile_url TEXT);
+    """)
+    
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS events(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        location TEXT,
+        starts_at TEXT,
+        ends_at TEXT,
+        categories TEXT,
+        price_from REAL,
+        url TEXT UNIQUE NOT NULL,
+        organiser_id INTEGER,
+        created_at TEXT,
+        FOREIGN KEY (organiser_id) REFERENCES organisers(id)
+        );
+    """)
+    
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users(
+        chat_id INTEGER PRIMARY KEY,
+        cities TEXT,
+        categories TEXT);
+    """)
+    
+    conn.commit()
+    
+if __name__ == "__main__":
+    with get_connection() as conn:
+        init_db(conn)
+    print("OK")
